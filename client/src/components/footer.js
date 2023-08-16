40d1c5f811d3c63ff32739c4da8456cf9f08edd3
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-08-16 17:22:05 -0400

    Added basic Footer.js structure

---
+++ client/src/components/footer.js
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div class="contactInfo">
+        <ul id="footerList">
+          <li>Cell: ?</li>
+          <li>
+            <a href="">Insta Link?</a>
+          </li>
+          <li>
+            <a href="">Other Soical?</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
