4f7b09977280ad1b884c318cdff9d96df1372099
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-09-13 14:29:58 -0600

    Added basis seed data for schema

---
+++ server/db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO reader (first_name, last_name, email)
+VALUES
+    ("Jaxson", "Lybbert", "jax.a.lybb@gmail.com"),
+    ("Stacy", "Lybbert", "lybbfam@sbcglobal.net"),
+    ("Chelsea", "Podcast Person", "123fakemail@yahoo.com"),
+    ("Taryn", "Other Podcast Person", "456fakemail@aol.com"),
