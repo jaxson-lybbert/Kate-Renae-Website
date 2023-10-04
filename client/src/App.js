3acee4f91e5cc0eef93f0058f1a8cdefddec2a12
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-10-04 12:07:59 -0600

    Added new pallette to repo

--- client/src/App.js
+++ client/src/App.js
@@ -8,23 +8,49 @@ import Home from "./pages/Home";
 import Stories from "./pages/Stories";
 import "./App.css";
 
+import { CssBaseline } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 function App() {
+  const theme = React.useMemo(() =>
+    createTheme({
+      palette: {
+        mode: "light",
+        primary: {
+          main: "#b15759",
+          light: "#EDEEC9",
+          dark: "#77BFA3",
+        },
+        secondary: {
+          main: "#BFD8BD",
+          dark: "#98C9A3",
+        },
+        background: {
+          default: "#f5f5f5",
+          paper: "#fafafa",
+        },
+      },
+    })
+  );
   return (
     <Router>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <div className="container">
-          {/* <Header /> */}
-          <Nav />
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/stories" element={<Stories />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<Home />} />
-          </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="flex-column justify-flex-start min-100-vh">
+          <div className="container">
+            {/* <Header /> */}
+            <Nav />
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/stories" element={<Stories />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<Home />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-      <Footer />
+        <Footer />
+      </ThemeProvider>
     </Router>
   );
 }
